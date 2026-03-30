abb756b3e3aec1c7c64075fb9cac22db1ca9be1d
Dhavy Sued <sueddhavy@gmail.com>
2026-03-30 18:07:49 -0300

    configura deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/jovensmja/',
   resolve: {
     extensions: ['.jsx', '.js', '.tsx', '.ts', '.mjs', '.json'],
   },
